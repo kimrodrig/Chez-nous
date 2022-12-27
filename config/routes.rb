55439148d2e2c314b073a39fe3fd17273c41ec3e
Kim <rodrigkim@gmail.com>
2022-12-27 14:34:32 -0500

    removed index for privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    resources :members, only: [:index, :create]
+    resources :members, only: [:create]
 
     post '/join', to: 'members#create'
-
 
     get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
