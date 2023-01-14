0a338ea846b08d2deeeabacb7846e2b8c9a6b6c1
Kim <rodrigkim@gmail.com>
2023-01-14 13:29:06 -0500

    fixed sizing problem

--- client/src/Home/Navbar.js
+++ client/src/Home/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
             <nav class="ml-2 mr-2">
                 <div class="max-w-screen-xl px-5 py-3 mx-auto md:px-6">
                     <div class="flex items-center">
-                        <ul class="flex-1 flex-row mt-0 px-8 space-x-8 font-['Lato'] text-xl italic py-2.5 rounded shadow-md">
+                        <ul class="flex-1 flex-row mt-0 space-x-8 font-['Lato'] text-xl italic ">
                             <NavLink exact="true" to="/" >
                                 signup
                             </NavLink>
