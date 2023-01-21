72cf8f691ae199a63da108ba7397e7a4a07ff741
Kim <rodrigkim@gmail.com>
2023-01-21 14:52:52 -0500

    added hyphenation to about

--- client/src/Home/About.js
+++ client/src/Home/About.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Hyphenated from 'react-hyphen';
 
 function About() {
   return (
     <div className="p-5 max-w-xl">
+      <Hyphenated>
         <p className="about-p">
           <em>chez nous</em> is a restaurant run out of a residential apartment. We—the two of us, Kevin and Kim—believe a lot of restaurant overhead is unnecessary and obstructive to gastronomy. Commercial real estate costs, excessive and outdated equipment, the archaic gratuity system, abundant waste: you are actually paying the hidden price for all of this when you eat out.
         </p>
@@ -16,7 +18,7 @@ function About() {
         </p>
         <br/>
         <p className="about-p">  
-          The dinner plus pairing costs $125. The way that we’re running the service requires parties to arrive on time, because cooking is timing, and when tardiness throws off the rhythm of plating it can actually affect other tables as well. So that’s our request: come open-minded, hungry, and on time. And bring cash. There’s a $25 deposit that needs to be paid in advance, and once you arrive, you’ll pay $120. Instructions regarding the deposit will be provided upon making a reservation. The exact location will also be disclosed at that point.
+          The dinner plus pairing costs $125. The way that we’re running the service requires parties to arrive on time, because cooking is timing, and when tardiness throws off the rhythm of plating it can actually affect other tables as well. So that’s our request: come open-minded, hungry, and on time. And bring cash. There’s a $25 deposit that needs to be paid in advance, and once you arrive, you’ll pay $100. Instructions regarding the deposit will be provided upon making a reservation. The exact location will also be disclosed at that point.
         </p>
         <br/>
         <p className="about-p">
@@ -26,6 +28,7 @@ function About() {
         <p className="about-p">
           Simply choose a time on our reservations page. We look forward to seeing you soon!
         </p>
+      </Hyphenated>
     </div>
     )
 }
