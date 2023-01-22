7cd52bc3b3939935d9c80bf64061eba2fe7d84cd
Kim <rodrigkim@gmail.com>
2023-01-22 14:05:26 -0500

    hover effects

--- client/src/Home/Navbar.js
+++ client/src/Home/Navbar.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { NavLink, useLocation} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-
-    const { pathname } = useLocation();
 
     return (
         <div class="navbar sticky top-0 z-50 bg-[#1e1e1e]"> 
@@ -11,19 +9,19 @@ function Navbar() {
                 <div 
                     class="px-5 lg:mx-32 md:mx-16 sm:mx-2 md:text-2xl py-3 flex items-center justify-between flex-row text-base italic tracking-wide"
                 >
-                    <NavLink to="/reservations" activeClassName="active">
+                    <NavLink to="/reservations" activeClassName="active" className="hvr-wobble-top">
                         reservations
                     </NavLink>
-                    <NavLink to="/menu" activeClassName="active">
+                    <NavLink to="/menu" activeClassName="active" className="hvr-wobble-top">
                         menu
                     </NavLink>   
-                    <NavLink to="/about" activeClassName="active">
+                    <NavLink to="/about" activeClassName="active" className="hvr-wobble-top">
                         about
                     </NavLink>
-                    <NavLink to="/gallery" activeClassName="active">
+                    <NavLink to="/gallery" activeClassName="active" className="hvr-wobble-top">
                         gallery
                     </NavLink>                   
-                    <NavLink to="/signup" activeClassName="active">
+                    <NavLink to="/signup" activeClassName="active" className="hvr-wobble-top">
                         subscribe
                     </NavLink>
                 </div>
